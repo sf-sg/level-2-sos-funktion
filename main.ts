ffd8dbea1a1faa8ffcb8784cb2bcd48444c2d291
sf-sg <62641897+sf-sg@users.noreply.github.com>
2020-03-25 09:27:42 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+function O () {
+    lang()
+    lang()
+    lang()
+    basic.pause(Zeichenende)
+}
+function S () {
+    for (let index = 0; index < 3; index++) {
+        kurz()
+    }
+    basic.pause(Zeichenende)
+}
+function kurz () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(Pause_Punkt)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(Pause_Punkt)
+}
+function lang () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(Pause_Strich)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(Pause_Punkt)
+}
+let Zeichenende = 0
+let Pause_Strich = 0
+let Pause_Punkt = 0
+Pause_Punkt = 200
+Pause_Strich = 600
+Zeichenende = 800
+basic.forever(function () {
+    S()
+    O()
+    S()
+    basic.pause(Zeichenende)
+})
